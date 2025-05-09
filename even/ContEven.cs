cef7c90e8f7cc020716a0134f0d3c02473d6d0c2
Maram suliman <mms.bur@outlook.com>
2025-05-09 17:52:52 +0400

    Add files via upload

---
+++ even/ContEven.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_maram
+{
+    public class CountEvens
+    {
+        public static void CountEvens(int[] numbers, ref int count)
+        {
+
+           count = 0;
+
+            Console.WriteLine("The even numbers :");
+
+            for (int i = 0; i < 20; i++)
+            {
+
+                if (numbers[i] % 2 == 0)
+                {
+
+                    count++;
+
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+
+            Console.WriteLine($"count of even numbers: {count}");
+        }
+    }
+
+}
---
+++ even/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace project_maram
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+    {
+       
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        
+        int evencount = 0;
+
+
+            CountEvens.CountEvens(numbers, ref evencount);
+
+        // evenCount now contains the number of even integers in the array
+        Console.WriteLine($"There are {evencount} even numbers in the array.");
+
+        Console.ReadLine(); // To keep the console window open
+    }
+}
+}
